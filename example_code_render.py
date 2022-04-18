649a8eda8612d7939c205d7ffceae8181aba8005
Hwaowoi <62416957+banhmikepxucxich@users.noreply.github.com>
2022-04-18 19:06:51 +0700

    screw it, im using pyqt6
    code render test added

--- example_code_render.py
+++ example_code_render.py
@@ -1,10 +1,18 @@
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout
+from PyQt6.QtWebEngineWidgets import QWebEngineView
+
+import codecs
 
 app = QApplication([])
 window = QWidget()
 window.setWindowTitle('code view test')
 
 layout = QVBoxLayout()
+
+code = QWebEngineView()
+html = codecs.open('test.html')
+code.setHtml(html.read())
+layout.addWidget(code)
 
 window.setLayout(layout)
 window.show()
