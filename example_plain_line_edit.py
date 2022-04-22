d08ae03d86ed02a140383b6d7df16c080c0180fa
Hwaowoi <62416957+banhmikepxucxich@users.noreply.github.com>
2022-04-22 23:33:28 +0700

    yes
    :)

---
+++ example_plain_line_edit.py
@@ -0,0 +1,32 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout
+from PyQt6.QtWidgets import QPushButton, QPlainTextEdit
+from PyQt6.QtGui import QFontMetrics, QFont, QFontDatabase
+from PyQt6.QtCore import QFile
+
+def example_function():
+    print(textbox.toPlainText().expandtabs(4))
+
+fonts = [
+    QFontDatabase.addApplicationFont(':/fonts/JetBrainsMono.ttf')
+]
+
+test = open('test.html')
+
+app = QApplication([])
+window = QWidget()
+window.setWindowTitle('example app :DDDDD')
+
+layout = QVBoxLayout()
+
+textbox = QPlainTextEdit()
+textbox.setFont(QFont('JetBrains Mono', 14))
+textbox.setTabStopDistance(QFontMetrics(textbox.font()).horizontalAdvance(' ') * 4)
+layout.addWidget(textbox)
+
+button = QPushButton('button')
+button.clicked.connect(example_function)
+layout.addWidget(button)
+
+window.setLayout(layout)
+window.show()
+app.exec()
